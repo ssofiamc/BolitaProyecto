0b772434050f481f9409993879a962da0d265f20
ssofiamc <159634938+ssofiamc@users.noreply.github.com>
2026-06-01 08:07:30 -0500

    Menú en otra escena

--- Assets/Scripts/Menu/MenuPausa.cs
+++ Assets/Scripts/Menu/MenuPausa.cs
@@ -1,24 +1,25 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuPausa : MonoBehaviour
 {
-    // Arrastra aquí tu panel de pausa desde el Inspector
-    public GameObject panelPausa;
     private bool juegoPausado = false;
 
-    // Esta función la llamará tu botón de Pausa
     public void Pausar()
     {
-        panelPausa.SetActive(true);    // Muestra el menú de pausa
         Time.timeScale = 0f;          // Congela el juego
         juegoPausado = true;
     }
 
-    // Esta función la llamará tu botón de Reanudar/Despausa
     public void Reanudar()
     {
-        panelPausa.SetActive(false);   // Esconde el menú de pausa
         Time.timeScale = 1f;          // Descongela el juego
         juegoPausado = false;
+    }
+
+    public void VolverAlJuego()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("GameScene"); 
     }
 }
